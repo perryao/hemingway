5dbc4cdf79a51cb4dabef2de923622505373817a
Mike Perry <mike@mikeperry.io>
2018-10-01 15:19:08 -0400

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ var includePaths = [
 ];
 
 gulp.task("default", function () {
-  gulp.src('sass/style.sass')
+  return gulp.src('sass/style.sass')
     .pipe(sass({outputStyle: 'compressed', includePaths: includePaths}))
     .pipe(autoprefixer())
     .pipe(gulp.dest('static/css/'));
 });
 
 gulp.task("watch", function () {
-  gulp.watch('sass/style.sass', function() {
+  return gulp.watch('sass/style.sass', function() {
     gulp.src('sass/style.sass')
       .pipe(sass({outputStyle: 'compressed', includePaths: includePaths}))
       .pipe(autoprefixer())
