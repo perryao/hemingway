1964b24048aa4f310237ac7426f389b0fb9c88c0
Mike Perry <mike@mikeperry.io>
2018-10-01 15:07:40 -0400

    add node_modules to sass import path

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
+var path = require('path');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var includePaths = [
+  path.join(__dirname, '/node_modules/'),
+];
 
 gulp.task("default", function () {
   gulp.src('sass/style.sass')
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({outputStyle: 'compressed', includePaths: includePaths}))
     .pipe(autoprefixer())
     .pipe(gulp.dest('static/css/'));
 });
@@ -12,7 +16,7 @@ gulp.task("default", function () {
 gulp.task("watch", function () {
   gulp.watch('sass/style.sass', function() {
     gulp.src('sass/style.sass')
-      .pipe(sass({outputStyle: 'compressed'}))
+      .pipe(sass({outputStyle: 'compressed', includePaths: includePaths}))
       .pipe(autoprefixer())
       .pipe(gulp.dest('static/css/'));
   });
